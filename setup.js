e424d801b819009d5f899b54ebdc4e6eaee40443
taey <tkul@sertiscorp.com>
2019-01-24 14:08:59 +0700

    change to async await

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const createDatabase = () => {
+const createDatabase = async () => {
   const pool = new Pool({
     user: 'postgres',
     password: 'postgres',
@@ -8,27 +8,26 @@ const createDatabase = () => {
     database: 'postgres',
     port: 5432,
   })
-  return new Promise((resolve, reject) => {
-    pool.connect((err, client, done) => {
-      if (err) throw err
-      client.query('CREATE DATABASE warehouse', (err, res) => {
-        if(res){
-          console.log(res);
-        }
-        if(err && err.code === "42P04"){
-          console.log("Database already exist");
-        }
-        else{
-          console.log(err);
-        }
-        done()
-        resolve()
-      })
-    })
-  });
+  const client = await pool.connect()
+  try {
+    const res = await client.query('CREATE DATABASE warehouse')
+    console.log(res);
+  }
+  catch (err) {
+    if(err && err.code === "42P04"){
+      console.log("Database already exist");
+    }
+    else{
+      console.log(err);
+    }
+  }
+  finally {
+    client.release()
+    return Promise.resolve()
+  }
 }
 
-const createTables = () => {
+const createTables = async () => {
   const pool = new Pool({
     user: 'postgres',
     password: 'postgres',
@@ -48,63 +47,23 @@ const createTables = () => {
         created_date TIMESTAMP,
         modified_date TIMESTAMP
       )`;
-
-  return new Promise((resolve, reject) => {
-    pool.connect((err, client, done) => {
-      if (err) throw err
-      client.query(queryText)
-        .then((res) => {
-          console.log(res);
-          console.log('done');
-          done()
-          resolve()
-        })
-        .catch((err) => {
-          console.log(err);
-          console.log('err');
-          done()
-          resolve()
-        })
-    })
-  });
-}
-
-const add = () => {
-  const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    database: 'warehouse',
-    port: 5432,
-  })
-  const queryText =
-    `INSERT INTO
-    users(email, password)
-    VALUES($1, $2)`;
-  return new Promise((resolve, reject) => {
-    pool.connect((err, client, done) => {
-      if (err) throw err
-      client.query(queryText, ["aaa@gmail.com", 123456])
-        .then((res) => {
-          console.log(res);
-          console.log('done');
-          done()
-          resolve()
-        })
-        .catch((err) => {
-          console.log(err);
-          console.log('err');
-          done()
-          resolve()
-        })
-    })
-  });
+  const client = await pool.connect()
+  try {
+    const res = await client.query(queryText)
+    console.log('create user table done');
+  }
+  catch (err) {
+    console.log(err);
+  }
+  finally {
+    client.release()
+    return Promise.resolve()
+  }
 }
 
 const setupDatabase = async () => {
   await createDatabase()
   await createTables()
-  await add()
 }
 
 setupDatabase()
