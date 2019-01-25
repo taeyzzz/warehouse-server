50b1f83c50a87a3ab1c5326b1b587dd75a382215
taey <tkul@sertiscorp.com>
2019-01-25 19:13:54 +0700

    WIP bcrypt  flow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,14 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
       PORT: 4000,
-      JWT_SECRET: "warehouse-secret101"
+      JWT_SECRET: "warehouse-secret101",
+      BCRYPT_SALT_ROUNDS: 10
     },
     env_production : {
       NODE_ENV: 'production',
       PORT: 4000,
-      JWT_SECRET: "warehouse-secret101"
+      JWT_SECRET: "warehouse-secret101",
+      BCRYPT_SALT_ROUNDS: 10
     }
   }]
 };
--- routers/user.js
+++ routers/user.js
@@ -1,30 +1,65 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const db = require('../db')
 
 const router = express.Router();
 
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body
+  const client = await db.connect()
+  try {
+    const result = await client.query(`select * from users where email = $1`, [email])
+    const same = await bcrypt.compare('password', result.rows[0].password)
+    if(!same){
+      throw new Error('password not same')
+    }
+    res.json({
+      msg: 'login'
+    })
+  }
+  catch (err) {
+    res.status(401).json({
+      msg: 'no tp ass'
+    })
+  }
+})
+
+
 router.get("/information", (req, res, next) => {
   const { token } = req.cookies
+  console.log(req.cookies);
   var decoded = jwt.verify(token, process.env.JWT_SECRET);
   console.log(decoded);
   res.json(decoded)
 })
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
   const data = {
     email,
     password
   }
-  var token = jwt.sign(data, process.env.JWT_SECRET, {
-    expiresIn: "60s"
-  });
-  res.cookie('token', token, { expires: new Date(Date.now() + 60000), httpOnly: true });
-  res.json({
-    email: req.body.email,
-    password: req.body.password
-  })
+  const client = await db.connect()
+  try {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS)
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hash = await bcrypt.hash(password, salt)
+    const result = await client.query(`INSERT into users (email, password) VALUES($1, $2)`, [email, hash])
+    console.log(result);
+    const token = jwt.sign(data, process.env.JWT_SECRET, {
+      expiresIn: "120s"
+    });
+    res.cookie('token', token, { expires: new Date(Date.now() + 60*60*1000), httpOnly: true });
+    res.json({
+      email: req.body.email,
+      password: req.body.password
+    })
+  }
+  catch (err) {
+    console.log(err);
+  }
 })
 
 module.exports = router
